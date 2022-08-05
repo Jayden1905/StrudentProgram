688affdb2c354a75f9245de39e21dc2072aa8458
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-08-05 21:23:31 +0630

    Complete CRUD student registration table with alerts and others condition stuffs, and still need to add input validation.

--- src/main/java/com/example/demo/Home.java
+++ src/main/java/com/example/demo/Home.java
@@ -11,6 +11,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class Home implements Initializable {
@@ -57,6 +58,8 @@ public class Home implements Initializable {
 
         studentList.add(new Student("Jayden", 19, "123456", "Male", "Diploma In Information Technology"));
         studentList.add(new Student("Alice", 22, "3245982", "Female", "Diploma In Business Management"));
+        studentList.add(new Student("Jordan", 20, "1945739", "Male", "Diploma In Robotic Engineering"));
+        studentList.add(new Student("Nike", 23, "3452678", "Female", "Diploma In Tourism"));
 
         studentTable.setItems(studentData);
 
@@ -83,14 +86,31 @@ public class Home implements Initializable {
         gender = stuGender.getValue();
         program = studentProgram.getText();
 
-        studentList.add(new Student(name, age, id, gender, program));
-        tempStudents.add(new Student(name, age, id, gender, program));
+        if (!Objects.equals(studentName.getText(), "") && !Objects.equals(studentAge.getText(), "") &&
+                !Objects.equals(stuGender.getValue(), "") &&
+                !Objects.equals(studentID.getText(), "") || !Objects.equals(studentProgram.getText(), "")) {
 
-        for (Student stu : tempStudents) {
-            studentTable.getItems().add(stu);
+            studentList.add(new Student(name, age, id, gender, program));
+            tempStudents.add(new Student(name, age, id, gender, program));
+
+            for (Student stu : tempStudents) {
+                studentTable.getItems().add(stu);
+            }
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText("Added the student");
+            alert.setContentText("The student " + name + " is added to the table.");
+            alert.show();
+
+            resetStudentForm();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setHeaderText("An Error Occurs");
+            alert.setContentText("You need to input data first!");
+            alert.show();
         }
-
-        resetStudentForm();
     }
 
     public void editData(Student stu) {
@@ -103,34 +123,64 @@ public class Home implements Initializable {
     }
 
     public Student updateData(Student stu) {
+
         selectedStudent = stu;
         selectedStudent.setStudentName(studentName.getText());
         selectedStudent.setStudentAge(Integer.parseInt(studentAge.getText()));
         selectedStudent.setStudentGender(stuGender.getValue());
         selectedStudent.setStudentID(studentID.getText());
         selectedStudent.setStudentProgram(studentProgram.getText());
+
         return stu;
     }
 
     public void insertStudent() {
-        editData(studentTable.getSelectionModel().getSelectedItem());
+        if (studentTable.getSelectionModel().getSelectedItem() != null) {
+            editData(studentTable.getSelectionModel().getSelectedItem());
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("WARNING");
+            alert.setHeaderText("Selection requires!");
+            alert.setContentText("You need to select the data from the table.");
+            alert.showAndWait();
+        }
     }
 
     public void updateStudent() {
-        studentTable.getItems().add(studentTable.getSelectionModel().getSelectedIndex(),
-                updateData(studentTable.getSelectionModel().getSelectedItem()));
-        studentTable.getItems().remove(studentTable.getSelectionModel().getSelectedIndex() - 1);
-        studentTable.refresh();
-        studentList.add(updateData(studentTable.getSelectionModel().getSelectedItem()));
-        studentList.remove(studentTable.getSelectionModel().getSelectedIndex());
-        resetStudentForm();
+        if (studentTable.getSelectionModel().getSelectedItem() != null) {
+            studentTable.getItems().add(studentTable.getSelectionModel().getSelectedIndex(),
+                    updateData(studentTable.getSelectionModel().getSelectedItem()));
+            studentTable.getItems().remove(studentTable.getSelectionModel().getSelectedIndex() - 1);
+            studentTable.refresh();
+
+            studentList.add(updateData(studentTable.getSelectionModel().getSelectedItem()));
+            studentList.remove(studentTable.getSelectionModel().getSelectedIndex());
+
+            resetStudentForm();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("WARNING");
+            alert.setHeaderText("Input requires!");
+            alert.setContentText("You need to select the data you want to update first. And then you can edit and update them!");
+            alert.show();
+        }
     }
 
     public void deleteStudent() {
-        studentList.remove(studentTable.getSelectionModel().getSelectedItem());
-        studentTable.getItems().remove(studentTable.getSelectionModel().getSelectedIndex());
-        studentTable.refresh();
-        resetStudentForm();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("ERROR");
+        alert.setHeaderText("Deleting!");
+        alert.setContentText("Do you want to delete it?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            studentList.remove(studentTable.getSelectionModel().getSelectedItem());
+
+            studentTable.getItems().remove(studentTable.getSelectionModel().getSelectedIndex());
+            studentTable.refresh();
+
+            resetStudentForm();
+        }
+
     }
 
     public void printStudents() {
